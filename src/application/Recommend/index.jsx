3143520ebeb4c7872fb155125806b9c042d866b1
搬砖达人小王git-flow <461864051@qq.com>
2022-10-31 09:23:19 +0800

    feat: swiper 1

--- src/application/Recommend/index.jsx
+++ src/application/Recommend/index.jsx
@@ -2,9 +2,18 @@
  * http://localhost:3000/#/recommend
  */
 import React from 'react';
+import Slider from '../../components/slider';
 
 function Recommend() {
-	return <div>Recommend</div>;
+	//mock 数据
+	const bannerList = [1, 2, 3, 4].map(item => {
+		return {
+			imageUrl:
+				'http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg',
+		};
+	});
+
+	return <Slider bannerList={bannerList}></Slider>;
 }
 
 export default React.memo(Recommend);
