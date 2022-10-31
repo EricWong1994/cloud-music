f7abcd5c8bc55496594a564947b27069fc50a28d
王石浩 <wangshihao@wjbrain.com>
2022-10-31 14:25:56 +0800

    feat: music list

--- src/application/Recommend/index.jsx
+++ src/application/Recommend/index.jsx
@@ -3,6 +3,7 @@
  */
 import React from 'react';
 import Slider from '../../components/slider';
+import RecommendList from '../../components/list';
 
 function Recommend() {
 	//mock 数据
@@ -13,7 +14,21 @@ function Recommend() {
 		};
 	});
 
-	return <Slider bannerList={bannerList}></Slider>;
+	const recommendList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(item => {
+		return {
+			id: 1,
+			picUrl: 'https://p1.music.126.net/fhmefjUfMD-8qtj3JKeHbA==/18999560928537533.jpg',
+			playCount: 17171122,
+			name: '朴树、许巍、李健、郑钧、老狼、赵雷',
+		};
+	});
+
+	return (
+		<div>
+			<Slider bannerList={bannerList}></Slider>
+			<RecommendList recommendList={recommendList}></RecommendList>
+		</div>
+	);
 }
 
 export default React.memo(Recommend);
