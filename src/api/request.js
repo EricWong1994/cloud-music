ea072c7b8b19cb52b70217d53f9188e6514dd1a9
王石浩 <wangshihao@wjbrain.com>
2022-11-01 18:34:25 +0800

    feat: 歌手页面

--- src/api/request.js
+++ src/api/request.js
@@ -7,3 +7,36 @@ export const getBannerRequest = () => {
 export const getRecommendListRequest = () => {
   return axiosInstance.get('/personalized');
 }
+
+export const getHotSingerListRequest = (count) => {
+  return axiosInstance.get(`/top/artists?offset=${count}`);
+}
+
+export const getSingerListRequest = (category, alpha, count) => {
+  return axiosInstance.get(`/artist/list?cat=${category}&initial=${alpha.toLowerCase()}&offset=${count}`);
+}
+
+export const categoryMap = new Map([
+  ['1001', { type: 1, area: 7 }],
+  ['1002', { type: 2, area: 7 }],
+  ['1003', { type: 3, area: 7 }],
+  ['2001', { type: 1, area: 96 }],
+  ['2002', { type: 2, area: 96 }],
+  ['2003', { type: 3, area: 96 }],
+  ['6001', { type: 1, area: 8 }],
+  ['6002', { type: 2, area: 8 }],
+  ['6003', { type: 3, area: 8 }],
+  ['7001', { type: 1, area: 16 }],
+  ['7002', { type: 2, area: 16 }],
+  ['7003', { type: 3, area: 16 }],
+  ['4001', { type: 1, area: 0 }],
+  ['4002', { type: 2, area: 0 }],
+  ['4003', { type: 3, area: 0 }],
+]);
+
+// TODO
+// export const getSingerListRequest = (type, area, alpha, offset) => {
+//   return axiosInstance.get(
+//     `/artist/list?type=${type}&area=${area}&initial=${alpha.toLowerCase()}&offset=${offset}`
+//   )
+// }
--- src/application/Singers/index.jsx
+++ src/application/Singers/index.jsx
@@ -1,40 +1,67 @@
 /**
  * http://localhost:3000/#/singers
  */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Horizen from '../../baseUI/horizen-item';
 import { categoryTypes } from '../../api/config';
 import { NavContainer } from './style';
 import { alphaTypes } from '../../api/config';
 import { List, ListItem, ListContainer } from './style';
 import Scroll from '../../baseUI/scroll';
+import {
+	getSingerList,
+	getHotSingerList,
+	changeEnterLoading,
+	changePageCount,
+	refreshMoreSingerList,
+	changePullUpLoading,
+	changePullDownLoading,
+	refreshMoreHotSingerList,
+} from './store/actionCreators';
+import { connect } from 'react-redux';
+import Loading from '../../baseUI/loading';
 
-function Singers() {
+function Singers(props) {
+	const {
+		singerList,
+		enterLoading,
+		pullUpLoading,
+		pullDownLoading,
+		pageCount,
+	} = props;
+
+	const {
+		getHotSingerDispatch,
+		updateDispatch,
+		pullDownRefreshDispatch,
+		pullUpRefreshDispatch,
+	} = props;
 	let [category, setCategory] = useState('');
 	let [alpha, setAlpha] = useState('');
 
+	useEffect(() => {
+		getHotSingerDispatch();
+		// eslint-disable-next-line
+	}, []);
+
 	let handleUpdateAlpha = val => {
 		setAlpha(val);
+		// TODO
+		updateDispatch(category, val);
 	};
 
 	let handleUpdateCatetory = val => {
 		setCategory(val);
+		// TODO
+		updateDispatch(val, category);
 	};
-
-	//mock 数据
-	const singerList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(item => {
-		return {
-			picUrl: 'https://p2.music.126.net/uTwOm8AEFFX_BYHvfvFcmQ==/109951164232057952.jpg',
-			name: '隔壁老樊',
-			accountId: 277313426,
-		};
-	});
 
 	// 渲染函数，返回歌手列表
 	const renderSingerList = () => {
+		const list = singerList ? singerList.toJS() : [];
 		return (
 			<List>
-				{singerList.map((item, index) => {
+				{list.map((item, index) => {
 					return (
 						<ListItem key={item.accountId + '' + index}>
 							<div className='img_wrapper'>
@@ -51,6 +78,14 @@ function Singers() {
 				})}
 			</List>
 		);
+	};
+
+	const handlePullUp = () => {
+		pullUpRefreshDispatch(category, alpha, category === '', pageCount);
+	};
+
+	const handlePullDown = () => {
+		pullDownRefreshDispatch(category, alpha);
 	};
 
 	return (
@@ -70,10 +105,63 @@ function Singers() {
 				/>
 			</NavContainer>
 			<ListContainer>
-				<Scroll>{renderSingerList()}</Scroll>
+				<Scroll
+					pullUp={handlePullUp}
+					pullDown={handlePullDown}
+					pullUpLoading={pullUpLoading}
+					pullDownLoading={pullDownLoading}
+				>
+					{renderSingerList()}
+				</Scroll>
+				{enterLoading ? <Loading></Loading> : null}
+				{/* <Loading show={enterLoading}></Loading> */}
 			</ListContainer>
 		</div>
 	);
 }
 
-export default React.memo(Singers);
+const mapStateToProps = state => ({
+	singerList: state.getIn(['singers', 'singerList']),
+	enterLoading: state.getIn(['singers', 'enterLoading']),
+	pullUpLoading: state.getIn(['singers', 'pullUpLoading']),
+	pullDownLoading: state.getIn(['singers', 'pullDownLoading']),
+	pageCount: state.getIn(['singers', 'pageCount']),
+});
+
+const mapDispatchToProps = dispatch => {
+	return {
+		getHotSingerDispatch() {
+			dispatch(getHotSingerList());
+		},
+		updateDispatch(category, alpha) {
+			dispatch(changePageCount(0)); //由于改变了分类，所以pageCount清零
+			dispatch(changeEnterLoading(true)); //loading，现在实现控制逻辑，效果实现放到下一节，后面的loading同理
+			dispatch(getSingerList(category, alpha));
+		},
+		// 滑到最底部刷新部分的处理
+		pullUpRefreshDispatch(category, alpha, hot, count) {
+			dispatch(changePullUpLoading(true));
+			dispatch(changePageCount(count + 1));
+			if (hot) {
+				dispatch(refreshMoreHotSingerList());
+			} else {
+				dispatch(refreshMoreSingerList(category, alpha));
+			}
+		},
+		//顶部下拉刷新
+		pullDownRefreshDispatch(category, alpha) {
+			dispatch(changePullDownLoading(true));
+			dispatch(changePageCount(0)); //属于重新获取数据
+			if (category === '' && alpha === '') {
+				dispatch(getHotSingerList());
+			} else {
+				dispatch(getSingerList(category, alpha));
+			}
+		},
+	};
+};
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(React.memo(Singers));
