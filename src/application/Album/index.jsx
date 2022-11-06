fd59165643c421a3bf3b531481cac80c3d03d76b
搬砖达人小王git-flow <461864051@qq.com>
2022-11-07 00:24:18 +0800

    feat: 16 注释 mock数据

--- src/application/Album/index.jsx
+++ src/application/Album/index.jsx
@@ -15,165 +15,164 @@ function Album(props) {
 	const [showStatus, setShowStatus] = useState(true);
 	const [title, setTitle] = useState('歌单');
 	const [isMarquee, setIsMarquee] = useState(false); //是否跑马灯
-	//mock 数据
-	const currentAlbum = {
-		creator: {
-			avatarUrl:
-				'http://p1.music.126.net/O9zV6jeawR43pfiK2JaVSw==/109951164232128905.jpg',
-			nickname: '浪里推舟',
-		},
-		coverImgUrl:
-			'http://p2.music.126.net/ecpXnH13-0QWpWQmqlR0gw==/109951164354856816.jpg',
-		subscribedCount: 2010711,
-		name: '听完就睡，耳机是天黑以后柔软的梦境',
-		tracks: [
-			{
-				name: '我真的受伤了',
-				ar: [{ name: '张学友' }, { name: '周华健' }],
-				al: {
-					name: '学友 热',
-				},
-			},
-			{
-				name: '我真的受伤了',
-				ar: [{ name: '张学友' }, { name: '周华健' }],
-				al: {
-					name: '学友 热',
-				},
-			},
-			{
-				name: '我真的受伤了',
-				ar: [{ name: '张学友' }, { name: '周华健' }],
-				al: {
-					name: '学友 热',
-				},
-			},
-			{
-				name: '我真的受伤了',
-				ar: [{ name: '张学友' }, { name: '周华健' }],
-				al: {
-					name: '学友 热',
-				},
-			},
-			{
-				name: '我真的受伤了',
-				ar: [{ name: '张学友' }, { name: '周华健' }],
-				al: {
-					name: '学友 热',
-				},
-			},
-			{
-				name: '我真的受伤了',
-				ar: [{ name: '张学友' }, { name: '周华健' }],
-				al: {
-					name: '学友 热',
-				},
-			},
-			{
-				name: '我真的受伤了',
-				ar: [{ name: '张学友' }, { name: '周华健' }],
-				al: {
-					name: '学友 热',
-				},
-			},
-			{
-				name: '我真的受伤了',
-				ar: [{ name: '张学友' }, { name: '周华健' }],
-				al: {
-					name: '学友 热',
-				},
-			},
-			{
-				name: '我真的受伤了',
-				ar: [{ name: '张学友' }, { name: '周华健' }],
-				al: {
-					name: '学友 热',
-				},
-			},
-			{
-				name: '我真的受伤了',
-				ar: [{ name: '张学友' }, { name: '周华健' }],
-				al: {
-					name: '学友 热',
-				},
-			},
-			{
-				name: '我真的受伤了',
-				ar: [{ name: '张学友' }, { name: '周华健' }],
-				al: {
-					name: '学友 热',
-				},
-			},
-			{
-				name: '我真的受伤了',
-				ar: [{ name: '张学友' }, { name: '周华健' }],
-				al: {
-					name: '学友 热',
-				},
-			},
-			{
-				name: '我真的受伤了',
-				ar: [{ name: '张学友' }, { name: '周华健' }],
-				al: {
-					name: '学友 热',
-				},
-			},
-			{
-				name: '我真的受伤了',
-				ar: [{ name: '张学友' }, { name: '周华健' }],
-				al: {
-					name: '学友 热',
-				},
-			},
-			{
-				name: '我真的受伤了',
-				ar: [{ name: '张学友' }, { name: '周华健' }],
-				al: {
-					name: '学友 热',
-				},
-			},
-			{
-				name: '我真的受伤了',
-				ar: [{ name: '张学友' }, { name: '周华健' }],
-				al: {
-					name: '学友 热',
-				},
-			},
-			{
-				name: '我真的受伤了',
-				ar: [{ name: '张学友' }, { name: '周华健' }],
-				al: {
-					name: '学友 热',
-				},
-			},
-			{
-				name: '我真的受伤了',
-				ar: [{ name: '张学友' }, { name: '周华健' }],
-				al: {
-					name: '学友 热',
-				},
-			},
-			{
-				name: '我真的受伤了',
-				ar: [{ name: '张学友' }, { name: '周华健' }],
-				al: {
-					name: '学友 热',
-				},
-			},
-			{
-				name: '我真的受伤了',
-				ar: [{ name: '张学友' }, { name: '周华健' }],
-				al: {
-					name: '学友 热',
-				},
-			},
-		],
-	};
+	//mock 数据。 10条时，数据太少，撑不开滚动条，20条可以
+	// const currentAlbum = {
+	// 	creator: {
+	// 		avatarUrl:
+	// 			'http://p1.music.126.net/O9zV6jeawR43pfiK2JaVSw==/109951164232128905.jpg',
+	// 		nickname: '浪里推舟',
+	// 	},
+	// 	coverImgUrl:
+	// 		'http://p2.music.126.net/ecpXnH13-0QWpWQmqlR0gw==/109951164354856816.jpg',
+	// 	subscribedCount: 2010711,
+	// 	name: '听完就睡，耳机是天黑以后柔软的梦境',
+	// 	tracks: [
+	// 		{
+	// 			name: '我真的受伤了',
+	// 			ar: [{ name: '张学友' }, { name: '周华健' }],
+	// 			al: {
+	// 				name: '学友 热',
+	// 			},
+	// 		},
+	// 		{
+	// 			name: '我真的受伤了',
+	// 			ar: [{ name: '张学友' }, { name: '周华健' }],
+	// 			al: {
+	// 				name: '学友 热',
+	// 			},
+	// 		},
+	// 		{
+	// 			name: '我真的受伤了',
+	// 			ar: [{ name: '张学友' }, { name: '周华健' }],
+	// 			al: {
+	// 				name: '学友 热',
+	// 			},
+	// 		},
+	// 		{
+	// 			name: '我真的受伤了',
+	// 			ar: [{ name: '张学友' }, { name: '周华健' }],
+	// 			al: {
+	// 				name: '学友 热',
+	// 			},
+	// 		},
+	// 		{
+	// 			name: '我真的受伤了',
+	// 			ar: [{ name: '张学友' }, { name: '周华健' }],
+	// 			al: {
+	// 				name: '学友 热',
+	// 			},
+	// 		},
+	// 		{
+	// 			name: '我真的受伤了',
+	// 			ar: [{ name: '张学友' }, { name: '周华健' }],
+	// 			al: {
+	// 				name: '学友 热',
+	// 			},
+	// 		},
+	// 		{
+	// 			name: '我真的受伤了',
+	// 			ar: [{ name: '张学友' }, { name: '周华健' }],
+	// 			al: {
+	// 				name: '学友 热',
+	// 			},
+	// 		},
+	// 		{
+	// 			name: '我真的受伤了',
+	// 			ar: [{ name: '张学友' }, { name: '周华健' }],
+	// 			al: {
+	// 				name: '学友 热',
+	// 			},
+	// 		},
+	// 		{
+	// 			name: '我真的受伤了',
+	// 			ar: [{ name: '张学友' }, { name: '周华健' }],
+	// 			al: {
+	// 				name: '学友 热',
+	// 			},
+	// 		},
+	// 		{
+	// 			name: '我真的受伤了',
+	// 			ar: [{ name: '张学友' }, { name: '周华健' }],
+	// 			al: {
+	// 				name: '学友 热',
+	// 			},
+	// 		},
+	// 		{
+	// 			name: '我真的受伤了',
+	// 			ar: [{ name: '张学友' }, { name: '周华健' }],
+	// 			al: {
+	// 				name: '学友 热',
+	// 			},
+	// 		},
+	// 		{
+	// 			name: '我真的受伤了',
+	// 			ar: [{ name: '张学友' }, { name: '周华健' }],
+	// 			al: {
+	// 				name: '学友 热',
+	// 			},
+	// 		},
+	// 		{
+	// 			name: '我真的受伤了',
+	// 			ar: [{ name: '张学友' }, { name: '周华健' }],
+	// 			al: {
+	// 				name: '学友 热',
+	// 			},
+	// 		},
+	// 		{
+	// 			name: '我真的受伤了',
+	// 			ar: [{ name: '张学友' }, { name: '周华健' }],
+	// 			al: {
+	// 				name: '学友 热',
+	// 			},
+	// 		},
+	// 		{
+	// 			name: '我真的受伤了',
+	// 			ar: [{ name: '张学友' }, { name: '周华健' }],
+	// 			al: {
+	// 				name: '学友 热',
+	// 			},
+	// 		},
+	// 		{
+	// 			name: '我真的受伤了',
+	// 			ar: [{ name: '张学友' }, { name: '周华健' }],
+	// 			al: {
+	// 				name: '学友 热',
+	// 			},
+	// 		},
+	// 		{
+	// 			name: '我真的受伤了',
+	// 			ar: [{ name: '张学友' }, { name: '周华健' }],
+	// 			al: {
+	// 				name: '学友 热',
+	// 			},
+	// 		},
+	// 		{
+	// 			name: '我真的受伤了',
+	// 			ar: [{ name: '张学友' }, { name: '周华健' }],
+	// 			al: {
+	// 				name: '学友 热',
+	// 			},
+	// 		},
+	// 		{
+	// 			name: '我真的受伤了',
+	// 			ar: [{ name: '张学友' }, { name: '周华健' }],
+	// 			al: {
+	// 				name: '学友 热',
+	// 			},
+	// 		},
+	// 		{
+	// 			name: '我真的受伤了',
+	// 			ar: [{ name: '张学友' }, { name: '周华健' }],
+	// 			al: {
+	// 				name: '学友 热',
+	// 			},
+	// 		},
+	// 	],
+	// };
 
 	const headerEl = useRef();
 
 	const { currentAlbum: currentAlbumImmutable, enterLoading } = props;
-	// const {enterLoading } = props;
 	const { getAlbumDataDispatch } = props;
 
 	const id = props.match.params.id;
@@ -182,8 +181,7 @@ function Album(props) {
 		getAlbumDataDispatch(id);
 	}, [getAlbumDataDispatch, id]);
 
-	// let currentAlbum = currentAlbumImmutable.toJS();
-	// console.log('currentAlbumImmutable.toJS(): ', currentAlbumImmutable.toJS());
+	let currentAlbum = currentAlbumImmutable.toJS();
 
 	const handleBack = useCallback(() => {
 		setShowStatus(false);
