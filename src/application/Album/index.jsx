384692860a3c1d804bee9948178f67daa3696f9e
搬砖达人小王git-flow <461864051@qq.com>
2022-11-06 22:24:01 +0800

    feat: 16-1 完善歌单详情

--- src/application/Album/index.jsx
+++ src/application/Album/index.jsx
@@ -1,10 +1,97 @@
 import React, { useState } from 'react';
-import { Container } from './style';
 import { CSSTransition } from 'react-transition-group';
 import Header from './../../baseUI/header/index';
+import Scroll from './../../baseUI/scroll/index';
+import { getName, getCount } from './../../api/utils';
+import { Container, TopDesc, Menu, SongList, SongItem } from './style';
 
 function Album(props) {
 	const [showStatus, setShowStatus] = useState(true);
+	//mock 数据
+	const currentAlbum = {
+		creator: {
+			avatarUrl:
+				'http://p1.music.126.net/O9zV6jeawR43pfiK2JaVSw==/109951164232128905.jpg',
+			nickname: '浪里推舟',
+		},
+		coverImgUrl:
+			'http://p2.music.126.net/ecpXnH13-0QWpWQmqlR0gw==/109951164354856816.jpg',
+		subscribedCount: 2010711,
+		name: '听完就睡，耳机是天黑以后柔软的梦境',
+		tracks: [
+			{
+				name: '我真的受伤了',
+				ar: [{ name: '张学友' }, { name: '周华健' }],
+				al: {
+					name: '学友 热',
+				},
+			},
+			{
+				name: '我真的受伤了',
+				ar: [{ name: '张学友' }, { name: '周华健' }],
+				al: {
+					name: '学友 热',
+				},
+			},
+			{
+				name: '我真的受伤了',
+				ar: [{ name: '张学友' }, { name: '周华健' }],
+				al: {
+					name: '学友 热',
+				},
+			},
+			{
+				name: '我真的受伤了',
+				ar: [{ name: '张学友' }, { name: '周华健' }],
+				al: {
+					name: '学友 热',
+				},
+			},
+			{
+				name: '我真的受伤了',
+				ar: [{ name: '张学友' }, { name: '周华健' }],
+				al: {
+					name: '学友 热',
+				},
+			},
+			{
+				name: '我真的受伤了',
+				ar: [{ name: '张学友' }, { name: '周华健' }],
+				al: {
+					name: '学友 热',
+				},
+			},
+			{
+				name: '我真的受伤了',
+				ar: [{ name: '张学友' }, { name: '周华健' }],
+				al: {
+					name: '学友 热',
+				},
+			},
+			{
+				name: '我真的受伤了',
+				ar: [{ name: '张学友' }, { name: '周华健' }],
+				al: {
+					name: '学友 热',
+				},
+			},
+			{
+				name: '我真的受伤了',
+				ar: [{ name: '张学友' }, { name: '周华健' }],
+				al: {
+					name: '学友 热',
+				},
+			},
+			{
+				name: '我真的受伤了',
+				ar: [{ name: '张学友' }, { name: '周华健' }],
+				al: {
+					name: '学友 热',
+				},
+			},
+		],
+	};
+
 	const handleBack = () => {
 		setShowStatus(false);
 	};
@@ -20,12 +107,104 @@ function Album(props) {
 			onExited={props.history.goBack}
 		>
 			{/* <Container>Album</Container>; */}
-			{/* <Container></Container>; */}
 			{/* 上面这一行在CSSTransition组件下多一个分号都会导致报错 */}
 			<Container>
 				<Header title={'返回'} handleClick={handleBack}></Header>
+				<Scroll bounceTop={false}>
+					<div>
+						<TopDesc background={currentAlbum.coverImgUrl}>
+							<div className='background'>
+								<div className='filter'></div>
+							</div>
+							<div className='img_wrapper'>
+								<div className='decorate'></div>
+								<img src={currentAlbum.coverImgUrl} alt='' />
+								<div className='play_count'>
+									<i className='iconfont play'>&#xe885;</i>
+									<span className='count'>
+										{Math.floor(
+											currentAlbum.subscribedCount / 1000
+										) / 10}{' '}
+										万{' '}
+									</span>
+								</div>
+							</div>
+							<div className='desc_wrapper'>
+								<div className='title'>{currentAlbum.name}</div>
+								<div className='person'>
+									<div className='avatar'>
+										<img
+											src={currentAlbum.creator.avatarUrl}
+											alt=''
+										/>
+									</div>
+									<div className='name'>
+										{currentAlbum.creator.nickname}
+									</div>
+								</div>
+							</div>
+						</TopDesc>
+						<Menu>
+							<div>
+								<i className='iconfont'>&#xe6ad;</i>
+								评论
+							</div>
+							<div>
+								<i className='iconfont'>&#xe86f;</i>
+								点赞
+							</div>
+							<div>
+								<i className='iconfont'>&#xe62d;</i>
+								收藏
+							</div>
+							<div>
+								<i className='iconfont'>&#xe606;</i>
+								更多
+							</div>
+						</Menu>
+					</div>
+					<SongList>
+						<div className='first_line'>
+							<div className='play_all'>
+								<i className='iconfont'>&#xe6e3;</i>
+								<span>
+									{' '}
+									播放全部{' '}
+									<span className='sum'>
+										(共 {currentAlbum.tracks.length} 首)
+									</span>
+								</span>
+							</div>
+							<div className='add_list'>
+								<i className='iconfont'>&#xe62d;</i>
+								<span>
+									{' '}
+									收藏 (
+									{getCount(currentAlbum.subscribedCount)})
+								</span>
+							</div>
+						</div>
+						<SongItem>
+							{currentAlbum.tracks.map((item, index) => {
+								return (
+									<li key={index}>
+										<span className='index'>
+											{index + 1}
+										</span>
+										<div className='info'>
+											<span>{item.name}</span>
+											<span>
+												{getName(item.ar)} -{' '}
+												{item.al.name}
+											</span>
+										</div>
+									</li>
+								);
+							})}
+						</SongItem>
+					</SongList>
+				</Scroll>
 			</Container>
-			{/* <Container></Container> */}
 		</CSSTransition>
 	);
 }
